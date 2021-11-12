a0224b71f9a2a766f91622b960f7b7e4e42cba6f
jdelrosa <djgabriel624@gmail.com>
2021-11-12 14:56:35 -0500

    Changes to DB functions.
    Minor additions to Db functions and adding a description to README.md.

---
+++ data/movies.js
@@ -0,0 +1,20 @@
+const mongoCollections = require('../config/mongoCollections');
+const movies = mongoCollections.movies;
+
+let create = async () => {
+    return 1; 
+};
+
+let update = async () => {
+    return 1; 
+};
+
+let remove = async () => {
+    return 1; 
+};
+
+module.exports = {
+    create,
+    update,
+    remove
+};
