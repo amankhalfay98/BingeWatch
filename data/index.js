f3673cf7e791341aa9d673111ef329a7363cf87c
amankhalfay98 <48147249+amankhalfay98@users.noreply.github.com>
2021-11-21 11:27:09 -0500

    added users.js

---
+++ data/index.js
@@ -0,0 +1,9 @@
+const moviesData = require('./movies');
+const usersData = require('./users');
+const reviewsData = require('./reviews');
+
+module.exports = {
+  restaurants: moviesData,
+  users: usersData,
+  reviews: reviewsData
+};
---
+++ notes/project-notes.md
@@ -0,0 +1,6 @@
+Working on user
+
+Tasks:
+Sign Up Page
+User Settings
+User Profile
