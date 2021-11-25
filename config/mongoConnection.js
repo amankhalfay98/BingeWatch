a3ced2006fed2213755ddc544fb9f877b522bbc4
amankhalfay98 <48147249+amankhalfay98@users.noreply.github.com>
2021-11-25 08:03:56 -0500

    Added Users data and routes

--- config/mongoConnection.js
+++ config/mongoConnection.js
@@ -5,16 +5,14 @@ const mongoConfig = settings.mongoConfig;
 let _connection = undefined;
 let _db = undefined;
 
-module.exports = {
-	connectToDb: async () => {
-	  if (!_connection) {
-		_connection = await MongoClient.connect(mongoConfig.serverUrl);
+module.exports = async () => {
+	if (!_connection) {
+		_connection = await MongoClient.connect(mongoConfig.serverUrl, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+		});
 		_db = await _connection.db(mongoConfig.database);
-	  }
-  
-	  return _db;
-	},
-	closeConnection: () => {
-	  _connection.close();
 	}
-  };
+
+	return _db;
+};
