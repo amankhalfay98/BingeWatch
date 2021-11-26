4242addb953ff9a3afab56dbcef1c21551def363
jdelrosa <djgabriel624@gmail.com>
2021-11-26 17:51:39 -0500

    Update movies.js
    Added error checking for create function

--- data/movies.js
+++ data/movies.js
@@ -1,8 +1,26 @@
 const mongoCollections = require('../config/mongoCollections');
 const movies = mongoCollections.movies;
 
-let create = async () => {
-    return 1; 
+let create = async (movie_name, director, release_year, cast, streaming, genre, movie_img) => {
+    if(!movie_name || !director || !release_year || !cast || !streaming || !genre || !movie_img) {
+        throw 'One or more input parameter missing.';
+    }
+
+    if(typeof movie_name !== 'string' || typeof director !== 'string') {
+        throw 'Incorrect data types';
+    }
+
+    if(typeof release_year !== 'number') {
+        throw 'Incorrect data type';
+    }
+
+    if(!Array.isArray(cast) || !Array.isArray(genre)) {
+        throw 'Incorrect data type';
+    }
+
+    //Do error check for obj and img
+
+
 };
 
 let update = async () => {
