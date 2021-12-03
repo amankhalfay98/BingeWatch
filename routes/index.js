eb4112288def4935e8e25d045ea684400fd80b2b
amankhalfay98 <48147249+amankhalfay98@users.noreply.github.com>
2021-12-03 03:54:31 -0500

    Added user routes and functions
    Added user functions
    Added user routes
    Added user login and signup pages
    Added error handling in data and routes

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,12 @@
 const userRoutes = require('./users');
-const reviewsRoutes = require("./reviews");
-
+const reviewsRoutes = require('./reviews');
 
 const constructorMethod = (app) => {
 	app.use('/', userRoutes);
-  app.use("/reviews", reviewsRoutes);
-
+	app.use('/reviews', reviewsRoutes);
 
 	app.use('*', (req, res) => {
-		res.status(404).json({ error: 'Not found' });
+		res.status(404).json({ error: 'Page Not found' });
 	});
 };
 
