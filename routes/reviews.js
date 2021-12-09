fe50cccaa0e7ecc9820e4c54bf48c09f5d64dc63
supersid <sid441.sg@gmail.com>
2021-12-09 00:47:43 -0500

    try catch added

--- routes/reviews.js
+++ routes/reviews.js
@@ -7,31 +7,38 @@ router.post("/:id", async (req, res) => {
   //let data = req.body;
   //const { movie_id } = data;
   //if(req.params.id){
-  const allReviews = await reviews.getReviewsByMovieId(req.params.id);
-  res.json(allReviews);
+  try {
+    const allReviews = await reviews.getReviewsByMovieId(req.params.id);
+    res.json(allReviews);
+  } catch (error) {}
   //}
 });
 router.post("/postReview", async (req, res) => {
-  let data = req.body;
-  const { user_id, username, movie_id, movie_name, review, rating, tag } = data;
-  const postReview = await reviews.create(
-    user_id,
-    username,
-    movie_id,
-    movie_name,
-    review,
-    rating,
-    tag
-  );
-  //   console.log(postReview);
+  try {
+    let data = req.body;
+    const { user_id, username, movie_id, movie_name, review, rating, tag } =
+      data;
+    const postReview = await reviews.create(
+      user_id,
+      username,
+      movie_id,
+      movie_name,
+      review,
+      rating,
+      tag
+    );
+    //   console.log(postReview);
+  } catch (error) {}
   res.json(postReview);
 });
 router.delete("/deleteReview/:id", async (req, res) => {
-  let data = req.params.id;
-  const deleteReview = await reviews.remove(data);
-  if (deleteReview.deleted) {
-    res.json(deleteReview);
-  }
+  try {
+    let data = req.params.id;
+    const deleteReview = await reviews.remove(data);
+    if (deleteReview.deleted) {
+      res.json(deleteReview);
+    }
+  } catch (error) {}
 });
 
 module.exports = router;
